9eb0a825746001a08fe87ab3a9faa33002c4bda6
prasantadebnath.2013@gmail.com <prasantadebnath.2013@gmail.com>
2022-02-22 23:58:15 +0530

    Prasanta: Basic typescript Project added

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import UserContainer from './components/UserContainer';
+import AboutUs from './components/AboutUs';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<UserContainer />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+    return (
+        <div className="container py-4">
+            <h1>Hello World !!</h1>
+        </div>
+    )
+}
+
+export default AboutUs;
