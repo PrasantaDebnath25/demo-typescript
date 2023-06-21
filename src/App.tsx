93e1c3745005838bb671c6acfe421e4b90967989
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-06-21 14:47:51 +0530

    import and setup for Toast Container

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,23 @@ import Header from "./components/Header";
 import UserContainer from './components/UserContainer';
 import AboutUs from './components/AboutUs';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App: React.FC = () => {
   return (
     <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Header />
       <Routes>
         <Route path="/" element={<UserContainer />} />
