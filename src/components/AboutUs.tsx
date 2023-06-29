7af72843949f0a0aca3256f96ab29f5cefac640a
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-06-29 14:27:01 +0530

    About Us page added

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,6 +2,7 @@ const AboutUs = () => {
     return (
         <div className="container py-4">
             <h1>Hello World !</h1>
+            <h1>This is blank page now</h1>
         </div>
     )
 }
